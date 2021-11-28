bf82bc147333fa216d9427b6c3a5f47a5d0b7184
Alexander Kachalov <Alexander_Kachalov@epam.com>
2021-11-28 15:12:53 +0300

    replace var with const

--- test/test.js
+++ test/test.js
@@ -9,15 +9,15 @@ async function siteHandler(){
   let driver = await new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
   await driver.get('https://www.handler.by');
   await driver.findElement(By.xpath("//*[contains(text(),'Получить консультацию')]")).click();
-  var query = driver.wait(until.elementLocated(By.css('input[name=form_text_27]')));
-  await query.sendKeys('Злой Тестировщик');
+  const inputName = driver.wait(until.elementLocated(By.css('input[name=form_text_27]')));
+  await inputName.sendKeys('Злой Тестировщик');
   await driver.findElement(By.css('input[name=form_text_28]')).sendKeys('123456789');
   await driver.findElement(By.name('web_form_submit')).click();
   await driver.sleep(3000);
   await driver.findElement(By.className('close jqmClose')).click();
   await driver.findElement(By.className('top-btn inline-search-show twosmallfont')).click();
   await driver.sleep(3000);
-  var query = driver.wait(until.elementLocated(By.id('title-search-input')));
-  await query.sendKeys('фурнитура', Key.ENTER);
+  const searchText = driver.wait(until.elementLocated(By.id('title-search-input')));
+  await searchText.sendKeys('системы', Key.ENTER);
 }
 siteHandler()
